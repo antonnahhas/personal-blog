f146451ec9f5bbb88b4642d5d480479462dead21
antonnahhas <anton-nahhas@hotmail.com>
2024-06-28 13:47:41 +0300

    Added two views contact and about along side welcome

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
