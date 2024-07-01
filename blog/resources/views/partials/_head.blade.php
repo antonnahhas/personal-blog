3597b966773237597a14e9eac027ad111ca4560c
antonnahhas <anton-nahhas@hotmail.com>
2024-07-01 15:58:40 +0300

    utilized blade to laravelize the html

---
+++ blog/resources/views/partials/_head.blade.php
@@ -0,0 +1,12 @@
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Laravel Blog @yield('title')</title>
+<!-- Other meta tags -->
+
+<!-- Include Bootstrap CSS from CDN for quick testing -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Include compiled CSS -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+@yield('stylesheets')
