8fa22bc924b8e8d79ed1afb3474ac660e9ee4455
antonnahhas <anton-nahhas@hotmail.com>
2024-07-01 14:14:36 +0300

    created main view and utilized blade to extend it to other views

--- blog/resources/views/pages/about.blade.php
+++ blog/resources/views/pages/about.blade.php
@@ -1,67 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Blog - About Me</title>
-    <!-- Other meta tags -->
-    
-    <!-- Include Bootstrap CSS from CDN for quick testing -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    
-    <!-- Include compiled CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand pl-3" href="\">Laravel blog</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+@extends('main')
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="\">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="\about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="\contact">Contact</a>
-                </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right pr-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        My account
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <div class="container pt-3">
+@section('content')
         <div class="row">
             <div class="col-md-12">
                 <h1>About Me</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, quae quos repellendus totam quas veritatis modi. Dicta nulla ipsa, quo, ea culpa iure minus quaerat reiciendis ut porro, est omnis.</p>
             </div>
         </div>
-    </div>
-
-
-    <!-- Include jQuery and Bootstrap JS from CDN for quick testing -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
-    <!-- Include compiled JS -->
-    <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@endsection
