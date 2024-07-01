eca23b69a16daec169b9501385f3789d7225577a
antonnahhas <anton-nahhas@hotmail.com>
2024-07-01 11:07:24 +0300

    added bootstrap and default bootstrap nav bar

--- blog/resources/js/bootstrap.js
+++ blog/resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
-window.axios = axios;
+window.Popper = require('@popperjs/core').default;
+window.$ = window.jQuery = require('jquery');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+require('bootstrap');
