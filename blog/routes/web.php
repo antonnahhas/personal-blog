482276d33d0cef2d80e904b81c07a2aed8cb9d61
antonnahhas <anton-nahhas@hotmail.com>
2024-07-02 14:31:45 +0300

    added a route to posts controller

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 
 Route::get('contact', [PagesController::class, 'getContact']);
-
 Route::get('about', [PagesController::class, 'getAbout']);
-
 Route::get('/', [PagesController::class, 'getIndex']);
+Route::resource('posts', PostController::class);
