401365fe39e8eb8f1ad5e71dff46fbe56658e478
antonnahhas <anton-nahhas@hotmail.com>
2024-07-03 16:43:04 +0300

    added a card in the show view with edit and delete buttons

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -4,6 +4,43 @@
 
 @section('content')
 
-<h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+        <div class="col-md-4">
+            <div class="card bg-light">
+                <div class="card-body">
+                    <dl>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <dt>Created at:</dt>
+                            </div>
+                            <div class="col-md-6">
+                                <dd>{{ date('M j, Y H:i', strtotime($post->created_at)) }}</dd>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <dt>Last updated:</dt>
+                            </div>
+                            <div class="col-md-6">
+                                <dd>{{ date('M j, Y H:i', strtotime($post->updated_at)) }}</dd>
+                            </div>
+                        </div>
+                    </dl>
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-6">
+                            {!! Html()->a(route('posts.edit', $post->id), 'Edit')->attribute('class', 'btn btn-primary btn-block') !!}
+                        </div>
+                        <div class="col-md-6">
+                        {!! Html()->a(route('posts.destroy', $post->id), 'Delete')->attribute('class', 'btn btn-danger btn-block') !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
