598043161d03cac7b3ec19f73d579d9c76e6e4e5
antonnahhas <anton-nahhas@hotmail.com>
2024-07-09 15:20:22 +0300

    declared relationships in post and category models

--- blog/app/Models/Category.php
+++ blog/app/Models/Category.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = "categories";
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
