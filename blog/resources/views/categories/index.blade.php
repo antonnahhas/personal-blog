d8c719a2ddd767ef11c6d46dbbe02ba502494e4f
antonnahhas <anton-nahhas@hotmail.com>
2024-07-09 16:42:48 +0300

    created a new table in categories view

--- blog/resources/views/categories/index.blade.php
+++ blog/resources/views/categories/index.blade.php
@@ -4,8 +4,27 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
-            
+        <div class="col-md-8">
+            <h1>Categories:</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($categories as $category)
+                    <tr>
+                        <th>{{ $category->id }}</th>
+                        <td>{{ $category->name }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-3">
+
         </div>
     </div>
 @endsection
